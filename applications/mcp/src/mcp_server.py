6133bbef51e1e379cea613cfe524167b5191ff66
mithileshnakade-ops <mithilesh.nakade@ellipsedata.com>
2026-01-14 10:22:54 +0530

    docker compose fixes

--- applications/mcp/src/mcp_server.py
+++ applications/mcp/src/mcp_server.py
@@ -17,7 +17,7 @@ MCP_SERVER_HOST = os.getenv("MCP_SERVER_HOST")
 MCP_SERVER_PORT = os.getenv("MCP_SERVER_PORT", "8080")
 
 # mcp = FastMCP("LLM_APP", host=MCP_SERVER_HOST, port=MCP_SERVER_PORT)
-mcp_app = FastMCP("LLM_APP", host="localhost", port=int(MCP_SERVER_PORT))
+mcp_app = FastMCP("LLM_APP", port=int(MCP_SERVER_PORT))
 
 
 # async def get_openai_response(prompt: str) -> str:
